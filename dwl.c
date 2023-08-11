585d94b78096a3af0871f3cd59aaaea6e8d13eaf
Chase Hadley <me@chadley.me>
2023-08-11 03:40:26 -0400

    apply center patch for v0.4

--- dwl.c
+++ dwl.c
@@ -127,7 +127,7 @@ struct Client {
 #endif
 	unsigned int bw;
 	uint32_t tags;
-	int isfloating, isurgent, isfullscreen, isterm, noswallow;
+	int iscentered, isfloating, isurgent, isfullscreen, isterm, noswallow;
 	uint32_t resize; /* configure serial of a pending resize */
 	pid_t pid;
 	Client *swallowing, *swallowedby;
@@ -220,6 +220,7 @@ typedef struct {
 	const char *id;
 	const char *title;
 	uint32_t tags;
+	int iscentered;
 	int isfloating;
 	char scratchkey;
 	int isterm;
@@ -498,6 +499,7 @@ applyrules(Client *c)
 	for (r = rules; r < END(rules); r++) {
 		if ((!r->title || strstr(title, r->title))
 				&& (!r->id || strstr(appid, r->id))) {
+			c->iscentered = r->iscentered;
 			c->isfloating = r->isfloating;
 			c->scratchkey = r->scratchkey;
 			c->isterm     = r->isterm;
@@ -509,6 +511,12 @@ applyrules(Client *c)
 					mon = m;
 		}
 	}
+
+	if (c->iscentered) {
+		c->geom.x = (mon->w.width - c->geom.width) / 2 + mon->m.x;
+		c->geom.y = (mon->w.height - c->geom.height) / 2 + mon->m.y;
+	}
+
 	wlr_scene_node_reparent(&c->scene->node, layers[c->isfloating ? LyrFloat : LyrTile]);
 	setmon(c, mon, newtags);
 }
